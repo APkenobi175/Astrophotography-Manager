3e453b31dd98fa689b71b86c4b6375a8d5d630d2
phippsammon <phippsammon175@gmail.com>
2026-05-16 11:52:36 -0600

    Updated CSRF_TRUSTED_ORIGINS

--- _server/_server/settings.py
+++ _server/_server/settings.py
@@ -32,9 +32,7 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 # Allow common local dev origins (e.g. Vite dev server) to make requests
 # without triggering Django's CSRF Origin check. Update or extend as needed.
-CSRF_TRUSTED_ORIGINS = [
-    os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
-]
+CSRF_TRUSTED_ORIGINS = ['http://10.0.0.197:8080']
 
 
 # Application definition
